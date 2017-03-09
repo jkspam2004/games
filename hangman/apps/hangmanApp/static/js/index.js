d6b09a0c6f04f83985ea02b600139826b112c371
jkspam2004 <jkspam2004@yahoo.com>
2017-03-09 09:58:07 -0800

    ensure difficulty level selected

--- hangman/apps/hangmanApp/static/js/index.js
+++ hangman/apps/hangmanApp/static/js/index.js
@@ -1,12 +1,23 @@
 (function () {
     $(document).ready(function() {
+        // toggle the level select form for kids/grown ups
         $('input[type=radio][name=gamelevel]').on('change', function() {
             $('.select_level').toggle();
         });
 
+        // ensure fields are entered/selected
         $('#index_form').on('submit', function(event){
+            var error = false;
+
             if ($.trim($("#name").val()) === "") {
-                $("#errors").append("Please enter a name");
+                $("#name_error").html("Enter a name");
+                error = true;
+            }
+            if ($.trim($("#level_batch1").val()) === "" && $.trim($("#level_batch2").val()) === "") {
+                $("#level_error").html("Select a level");
+                error = true;
+            }
+            if (error) {
                 return false;
             }
         });
